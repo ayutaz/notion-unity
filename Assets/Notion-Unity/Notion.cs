a06d6ee579fc14b4ef3e2afb6aa9c61b526f2e47
ayutaz <ka1357amnbpdr@gmail.com>
2022-07-04 01:38:28 +0900

    fix: notion api version

--- Assets/Notion-Unity/Notion.cs
+++ Assets/Notion-Unity/Notion.cs
@@ -6,7 +6,7 @@ namespace NotionUnity
     /// <summary>https://developers.notion.com/docs/getting-started</summary>
     public static class Notion
     {
-        public const string API_VERSION = "2021-08-16";
+        public const string API_VERSION = "2022-02-22";
         public const string API_URL = "https://api.notion.com/v1";
         public const bool DISPLAY_JSON_RESULT = false;
         public const string API_TOKEN = "secret_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
