7e7552a6612ae150db0adbdaef201ac4b55d5d6b
ayutaz <ka1357amnbpdr@gmail.com>
2022-07-04 01:35:07 +0900

    feat: export unitypackage editor menu

---
+++ Assets/Editor/ExportUnityPackage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+
+namespace Editor
+{
+    public static class ExportUnityPackage
+    {
+        [MenuItem("Unity-Notion/Export Unity Package")]
+        private static void ExportPackage()
+        {
+            AssetDatabase.ExportPackage(
+                new string[]
+                {
+                    "Assets/Notion-Unity",
+                },
+                "Notion-Unity.unitypackage",
+                ExportPackageOptions.Recurse |
+                ExportPackageOptions.IncludeDependencies
+            );
+        }
+    }
+}
